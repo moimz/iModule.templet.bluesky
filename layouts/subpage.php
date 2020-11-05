40483a74d1962a51372bf8a40a5ed54852a04d24
arzzcom <arzz@arzz.com>
2020-11-05 21:19:38 +0900

    서브페이지 숨김기능 적용

--- layouts/subpage.php
+++ layouts/subpage.php
@@ -8,7 +8,7 @@
  * @author Arzz (arzz@arzz.com)
  * @license MIT License
  * @version 1.0.0
- * @modified 2018. 1. 29.
+ * @modified 2020. 11. 5.
  */
 if (defined('__IM__') == false) exit;
 ?>
@@ -31,21 +31,12 @@ if (defined('__IM__') == false) exit;
 				<h2><?php echo $IM->getMenus($IM->menu)->title; ?></h2>
 				
 				<ul>
-					<?php foreach ($IM->getPages($IM->menu) as $item) { ?>
-					<?php if ($item->menu == 'writing' && $item->page == 'clinic1') { ?>
-					<li class="group<?php echo preg_match('/^clinic[1-4]$/',$IM->page) == true ? ' opened selected"' : ''; ?>">
-						<button type="button">글쓰기 클리닉<i class="fa fa-angle-down"></i></button>
-						<ul>
-					<?php } ?>
+					<?php foreach ($IM->getPages($IM->menu) as $item) { if ($item->is_hide == true) continue; ?>
 					<li<?php echo $IM->page == $item->page ? ' class="selected"' : ''; ?>>
 						<a href="<?php echo $IM->getUrl($item->menu,$item->page,false); ?>">
 							<?php echo $item->title; ?>
 						</a>
 					</li>
-					<?php if ($item->menu == 'writing' && $item->page == 'clinic4') { ?>
-						</ul>
-					</li>
-					<?php } ?>
 					<?php } ?>
 				</ul>
 			</nav>
